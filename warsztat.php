60b995566aff9ddcdb14a095e9ffc73714e65d46
Cezary Buras <s173152@v-ie.uek.krakow.pl>
2014-11-10 12:19:48 +0100

    dodanie zadania dom 1

---
+++ warsztat.php
@@ -0,0 +1,48 @@
+<?php
+
+include_once('samochod.php');
+
+				/*$mojSamochod = new Samochod();
+				$mojSamochod->Samochody('Seat','Ibiza','czarny','sedan');
+				echo $mojSamochod->helloSamochod();
+				$innySamochod = new Samochod();
+				$innySamochod->Samochody('Ford','Escort','zielony','hatchback');
+				echo $innySamochod->helloSamochod();
+				echo $innySamochod->kolorSamochod();
+				$innySamochod->Samochody('Ford','Escort','czerwony','hatchback');
+				echo $innySamochod->helloSamochod();
+
+				
+				
+
+				function otoSamochod(Samochod $s){
+					$opis="otoSamochod".$s->getMarka()."\n";
+					return $opis;
+				}
+
+				$samochod = new Samochod();
+				$samochod->otoSamochod($marka);
+				echo otoSamochod($samochod);
+*/
+				$mojSamochod = new Samochod('Seat','Ibiza','czarny','sedan');
+				echo $mojSamochod->helloSamochod();
+				
+				$innySamochod = new Samochod("Ford", "Escort", "Czerwony", "Sedan");
+				echo $innySamochod->helloSamochod();
+
+				function otoSamochod(Samochod $s){
+					$opis="otoSamochod ".$s->getMarka()."\n";
+					return $opis;
+				}
+
+            
+				echo otoSamochod($mojSamochod);
+				echo otoSamochod($innySamochod);
+
+
+				//$liczba_aut = Samochod::$licznik;
+				//echo $liczba_aut
+
+
+
+?>
